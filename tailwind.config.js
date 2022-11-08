57b3f56accf6791e1d862f883ee5901cbafca634
eldercs <deny_1999@Ukr.net>
2022-11-08 22:43:39 +0300

    add desktop version

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,24 +9,51 @@ module.exports = {
         'yellow-green': '#C79D09',
         'auth-color':'#F5F6F8',
         'grey-color': '#8890A6',
-        'border-color': '#C2CFE0'
+        'border-color': '#C2CFE0',
+        'border-colorxl':'#EBEFF2',
+        'desktop-text': '#192A3E',
+        'desktop-text2': '#333333'
       },
       fontSize:{
         '25': '25px',
-        '32': '32px'
+        '26': '1.625rem',
+        '32': '32px',
+        '40': '40px'
       },
       height:{
-        '700': '700px'
+        '50': '3.125rem',
+        '700': '700px',
+        '900': '900px'
+      },
+      width:{
+        '59':'59.7%',
+        '392': '24.5rem',
+        '860': '860px'
+      },
+      maxWidth:{
+        '394': '24.625rem',
       },
       minWidth:{
         '375': '375px'
       },
       spacing:{
-        '120': '120px'
+        '18': '1.125rem',
+        '26': '1.625rem',
+        '71': '71px',
+        '120': '120px',
+        '181': '181px'
+      },
+      margin:{
+        '21': '1.3125rem',
+        '91': '91px',
+        '113': '7rem',
+        '123': '7.6875rem',
+        '243': '15.18rem'
       },
       screens:{
+        md: '768px',
         xl: '1440px'
-      }
+      }, 
     },
     fontFamily:{
       'SF':['SF Pro Display']
